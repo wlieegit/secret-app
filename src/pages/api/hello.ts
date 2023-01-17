05e72bc709279b8e27d7d64cf95165ae275a8cc2
will.li <will.li>
2023-01-17 08:32:24 +0000

    feat: api validate login

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 
 type Data = {
-  name: string
-}
+  name?: string;
+  message?: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(403).json({
+      message: "401 Unauthorized",
+    });
+  }
+  res.status(200).json({ name: "John Doe" });
 }

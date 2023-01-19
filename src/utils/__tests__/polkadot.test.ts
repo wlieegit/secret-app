7e614816e48090c1574b8723bf10251ed72bd79c
will.li <will.li>
2023-01-19 12:03:38 +0000

    test(polkadot): let keyring sign a message instead of hardcore add tests for isValidMessage

--- src/utils/__tests__/polkadot.test.ts
+++ src/utils/__tests__/polkadot.test.ts
@@ -1,5 +1,6 @@
-import {isValidAddress, isValidSignature} from '../polkadot'
-import {stringToHex} from '@polkadot/util'
+import {isValidAddress, isValidMessage, isValidSignature} from '../polkadot'
+import {stringToHex, u8aToHex} from '@polkadot/util'
+import Keyring from '@polkadot/keyring'
 
 describe('polkadot', () => {
   describe('isValidAddress', () => {
@@ -30,10 +31,14 @@ describe('polkadot', () => {
 
   describe('isValidSignature', () => {
     it('should return true if signature is valid', async () => {
+      const keyring = new Keyring()
+      const user = keyring.addFromUri('//user')
+      const msg = 'Sign-in request for address 14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo.'
+      const signature = u8aToHex(user.sign(msg))
       const isValid = await isValidSignature(
-        'Sign-in request for address 14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo.',
-        '0xfc03197bd2110f613677913e3d52afbc1ecda9099109f01300a97acde7122d305d87d115cf173632319c6666d829a4585a45462cb3d2df5513f7d5a68c9f1785',
-        '14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo',
+        msg,
+        signature,
+        user.address,
       )
       expect(isValid).toBeTruthy()
     })
@@ -52,6 +57,20 @@ describe('polkadot', () => {
         'Sign-in request for address 14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo.',
         '0xfc03197bd2110f613677913e3d52afbc1ecda9099109f01300a97acde7122d305d87d115c',
         '14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo',
+      )
+      expect(isValid).toBeFalsy()
+    })
+  })
+  describe('isValidMessage', () => {
+    it('should return true if message is valid', async () => {
+      const isValid = await isValidMessage("Sign-in request for address 14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo.",
+      '14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo',
+      )
+      expect(isValid).toBeTruthy()
+    })
+    it('should return false if message is invalid', async () => {
+      const isValid = await isValidMessage("Sign-in request for address 14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84CCCC.",
+      '14GgSVJ1unwjVw4CuMGXYz4P4yT1HzVqEDEiExhiCS84EGQo',
       )
       expect(isValid).toBeFalsy()
     })
